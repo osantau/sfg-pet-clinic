c399438fe9fb8c894a8b882cd18808831b96d9d8
Octavian Santau <octavian.santau.os@gmail.com>
2020-07-12 11:55:14 +0300

    Add owner details. closes #53

--- pet-clinic-web/src/main/java/guru/springframework/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/controllers/OwnerController.java
@@ -2,7 +2,10 @@ package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import guru.springframework.services.OwnerService;
 
@@ -28,5 +31,14 @@ public class OwnerController {
 	@RequestMapping("/find")
 	public String findOwners() {
 		return "notimplemented";
+	}
+	
+	@GetMapping("/{ownerId}")
+	public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+		ModelAndView mav = new ModelAndView("owners/ownerDetails");
+		mav.addObject(ownerService.findById(ownerId));
+		mav.addObject("pets", ownerService.findById(ownerId).getPets());
+		return mav;
+		
 	}
 }
